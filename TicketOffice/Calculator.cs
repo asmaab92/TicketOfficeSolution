93f3a64691f4e09160a376a8370a3ade4fd07ada
Asma <asmaa.abualkheir@gmail.com>
2023-09-26 22:37:54 +0200

    Add project files.

---
+++ TicketOffice/Calculator.cs
@@ -0,0 +1,59 @@
+﻿namespace TicketOffice
+{
+    public class Calculator
+    {
+        public static void Main(string[] args)
+        {
+            int age = 0;
+            string place = null;
+            PriceSetter(age, place);
+            
+        }
+        
+        
+        public static int PriceSetter(int age, string place)
+        {
+            Console.WriteLine("Enter your age, please ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("If you want a seated ticket type 'seated', if you want a standing ticket type 'standing'");
+            place = Console.ReadLine();
+
+            int price = 0;
+
+            if( age <= 11 && place == "seated")
+            {
+                price = 50;
+                
+                
+            }
+            else if ( age <= 11 && place == "standing")
+            {
+                price = 25;
+            }
+            else if (age >= 12 && age <= 64 && place == "seated")
+            {
+                price = 170;
+            }
+            else if (age >= 12 && age <= 64 && place == "standing")
+            {
+                price = 110;
+            }
+            else if (age >= 65 && place == "seated")
+            {
+                price = 100;
+            }
+            else if (age >= 65 && place == "standing")
+            {
+                price = 60;
+            }
+            Console.WriteLine("The price of the ticket is: " + price + "SEK");
+            return price;
+        }
+        
+
+
+        
+
+    }
+}
